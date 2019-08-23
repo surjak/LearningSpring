1cccf6ae7a1c219162ec25c70a9dd8338eab0abb
surjak <surjak1999@gmail.com>
2019-08-23 09:02:54 +0200

    Initial Commit 2

--- src/main/java/com/application/clinic/ClinicApplication.java
+++ src/main/java/com/application/clinic/ClinicApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ClinicApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(ClinicApplication.class, args);
     }
