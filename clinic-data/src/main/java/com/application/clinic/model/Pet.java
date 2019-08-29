b09f5cbdfc9ca9c2fb6dfe374bbb40e1a91db6ef
surjak <surjak1999@gmail.com>
2019-08-29 13:57:07 +0200

    Updated, adding JPA entity

--- clinic-data/src/main/java/com/application/clinic/model/Pet.java
+++ clinic-data/src/main/java/com/application/clinic/model/Pet.java
@@ -1,11 +1,25 @@
 package com.application.clinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name="owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
 
     public String getName() {
         return name;
@@ -15,7 +29,7 @@ public class Pet extends BaseEntity {
         this.name = name;
     }
 
-    private LocalDate birthDate;
+
 
     public PetType getPetType() {
         return petType;
