6154c64d5da751dd235b264bec6d07db8075fd8f
surjak <surjak1999@gmail.com>
2019-09-01 14:46:31 +0200

    Upgraded to JUnit 5 and changed pom.xml, currently all works, closes #4

--- clinic-web/src/test/java/clinic/ClinicApplicationTests.java
+++ clinic-web/src/test/java/clinic/ClinicApplicationTests.java
@@ -1,12 +1,14 @@
 package clinic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import com.application.clinic.ClinicApplication;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = ClinicApplication.class)
 public class ClinicApplicationTests {
 
     @Test
