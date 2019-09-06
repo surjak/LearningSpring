0c6302fa1272a7fe3a7fe0c3278072fc58b22e9c
surjak <surjak1999@gmail.com>
2019-09-06 09:16:23 +0200

    Fix display bug, closes #16

--- clinic-web/src/main/java/com/application/clinic/controllers/OwnerController.java
+++ clinic-web/src/main/java/com/application/clinic/controllers/OwnerController.java
@@ -3,7 +3,11 @@ package com.application.clinic.controllers;
 import com.application.clinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
@@ -23,5 +27,12 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwners(){
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable Long ownerId){
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return  mav;
     }
 }
