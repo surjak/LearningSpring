001bb283cdc08bcbc9ca76ecd70b2d139518091e
surjak <surjak1999@gmail.com>
2019-09-09 15:19:06 +0200

    added json endpoint

--- clinic-web/src/main/java/com/application/clinic/controllers/VetController.java
+++ clinic-web/src/main/java/com/application/clinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.application.clinic.controllers;
 
+import com.application.clinic.model.Vet;
 import com.application.clinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -14,9 +19,13 @@ public class VetController {
     }
 
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
-    public String listVets(Model model)
-    {
+    public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
