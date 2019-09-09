321ae401a727cb465112e6e13140335740b03a18
surjak <surjak1999@gmail.com>
2019-09-09 15:10:52 +0200

    Added date format and tests

--- clinic-data/src/main/java/com/application/clinic/model/Pet.java
+++ clinic-data/src/main/java/com/application/clinic/model/Pet.java
@@ -1,6 +1,7 @@
 package com.application.clinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -38,6 +39,7 @@ public class Pet extends BaseEntity {
     @JoinColumn(name = "owner_id")
     private Owner owner;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Column(name = "birth_date")
     private LocalDate birthDate;
 
